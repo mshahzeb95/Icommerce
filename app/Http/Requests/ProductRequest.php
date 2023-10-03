739fa9f82b29d4919c2537ea1c94c2a223212106
Muhammad Shahzeb <77158069+mshahzeb95@users.noreply.github.com>
2023-10-03 16:58:54 +0500

    composer

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:2000'],
+            'images.*' => ['nullable', 'image'],
+            'deleted_images.*' => ['nullable', 'int'],
+            'image_positions.*' => ['nullable', 'int'],
+            'price' => ['required', 'numeric'],
+            'quantity' => ['nullable', 'numeric'],
+            'description' => ['nullable', 'string'],
+            'published' => ['required', 'boolean']
+        ];
+    }
+}
